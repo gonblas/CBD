9aa556d9559980d297d6867929479ca52cafbc16
Gonzalo Gabino Blasco <gonzagabino@gmail.com>
2024-03-21 16:22:15 -0300

    Practica 1 - Ejercicio 7 Arreglado

--- Practicas/Practica1/Ej7/ej7.pas
+++ Practicas/Practica1/Ej7/ej7.pas
@@ -1,9 +1,9 @@
 program ej7;
-uses Sysutils;
+uses sysutils;
 
 type
     student_type = record 
-        DNI: integer;
+        DNI: longint;
         id_file: string;
         full_name: string;
         address: string;
@@ -20,12 +20,11 @@ var
     text_file: text;
 begin
     assign(text_file, text_filename); reset(text_file);
-    assign(bin_file, '../tmp/alumnos.dat'); rewrite(bin_file);
-
-    while not eof(text_file) do
+    assign(bin_file, '../tmp/students.dat'); rewrite(bin_file);
+    while (not eof(text_file)) do
     begin
-        readln(text_file, student.DNI, student.id_file);
-        writeln(student.DNI, '   ',  student.id_file);
+        readln(text_file, student.DNI);
+        readln(text_file, student.id_file);
         readln(text_file, student.full_name);
         readln(text_file, student.address);
         readln(text_file, student.current_year, student.date_of_birth);
@@ -33,6 +32,7 @@ begin
         write(bin_file, student);
     end;
 
+    close(bin_file);
     close(text_file);
 end;
 
@@ -41,15 +41,16 @@ var
     student: student_type;
     choice: char;
 begin
-    seek(bin_file, 0);
+    assign(bin_file, '../tmp/students.dat'); reset(bin_file);
     WriteLn('Enter a char for searching students: ');
     read(choice);
     while(not eof(bin_file))do
     begin
         read(bin_file, student);
         if(student.full_name[1] = choice)then
-            WriteLn(student.full_name, ', ', student.DNI,', ', student.id_file, ', ', student.address, ', ', student.current_year, ', ', student.date_of_birth, '.');
+            WriteLn(student.full_name, ' | ', student.id_file, ' | ', student.current_year, ' | ', student.date_of_birth);
     end;
+    close(bin_file);
 end;
 
 
@@ -58,51 +59,42 @@ var
     student: student_type;
     text_file: text;
 begin
-    Assign(text_file, 'alumnosAEgresar.txt'); Rewrite(text_file);
-    seek(bin_file, 0);
+    assign(bin_file, '../tmp/students.dat'); reset(bin_file);
+    Assign(text_file, '../tmp/students_to_graduate.txt'); Rewrite(text_file);
     while(not eof(bin_file))do
     begin
         read(bin_file, student);
         if(student.current_year = 5)then
             begin
-                writeln(text_file, student.DNI, student.id_file, student.full_name);
+                writeln(text_file, student.DNI);
+                writeln(text_file, student.id_file);
+                writeln(text_file, student.full_name);
                 writeln(text_file, student.address);
-                writeln(text_file, student.current_year, student.date_of_birth);
+                writeln(text_file, student.date_of_birth);
             end;
             
     end;
+    close(bin_file);
+    close(text_file);
 end;
 
 
 procedure add_student(var bin_file: Student_tfile);
 var
     student: student_type;
-    input: string;
 begin
+    assign(bin_file, '../tmp/students.dat'); reset(bin_file);
     seek(bin_file, FileSize(bin_file));
     
-    writeln('Enter student DNI:');
-    readln(input);
-    student.DNI := StrToIntDef(input, 0);
-    
-    writeln('Enter student id file:');
-    readln(student.id_file);
-    
-    writeln('Enter student full name:');
-    readln(student.full_name);
-    
-    writeln('Enter student address:');
-    readln(student.address);
-    
-    writeln('Enter student current year:');
-    readln(input);
-    student.current_year := StrToIntDef(input, 0);
-    
-    writeln('Enter student date of birth:');
-    readln(input);
-    student.date_of_birth := StrToIntDef(input, 0);
-    
+    writeln('Enter student DNI:'); readln(student.DNI);
+    writeln('Enter student id file:'); readln(student.id_file);
+    writeln('Enter student full name:'); readln(student.full_name);
+    writeln('Enter student address:'); readln(student.address);
+    writeln('Enter student current year:'); readln(student.current_year);
+    writeln('Enter student date of birth:'); readln(student.date_of_birth);
     write(bin_file, student);
+
+    close(bin_file);
 end;
 
 
@@ -114,6 +106,7 @@ var
     found_id: boolean;
     new_year: integer;
 begin
+    assign(bin_file, '../tmp/students.dat'); reset(bin_file);
     repeat
         write('Enter student ID file to modify: ');
         readln(search_id);
@@ -122,17 +115,17 @@ begin
     found_id := false;
 
     seek(bin_file, 0);
-    while not eof(bin_file) do
+    while (not eof(bin_file) and (not found_id)) do
     begin
         read(bin_file, student);
         if student.id_file = search_id then
         begin
             found_id := true;
 
-            writeln('~~~Enter new current year (or leave blank to keep unchanged)~~~');
+            writeln('~~~Enter new current year~~~');
             write('Current year (current:', student.current_year, '): ');
             readln(new_year);
-            if new_year <> student.current_year then
+            if (new_year <> student.current_year) then
             begin
                 student.current_year := new_year;
                 seek(bin_file, FilePos(bin_file) - 1);
@@ -141,7 +134,6 @@ begin
             end
         end;
     end;
-    seek(bin_file, FileSize(bin_file));
     close(bin_file);
 
     if not found_id then
@@ -152,10 +144,9 @@ end;
 var
     bin_file: Student_tfile;
 begin
-    text_to_bin(bin_file, '../tmp/alumnos.txt');
-    // list_students(bin_file);
-    // list_students_on_file(bin_file);
-    // add_student(bin_file);
-    // modifyStudent(bin_file);
-    // close(bin_file);
+    text_to_bin(bin_file, '../tmp/students.txt');
+    list_students(bin_file);
+    list_students_on_file(bin_file);
+    add_student(bin_file);
+    modifyStudent(bin_file);
 end.
